e8024c6e42fffac118fe30af54c9bb04225802d9
Bowon Han <bowonhan@Bowonui-MacBookPro.local>
2023-10-06 14:18:04 +0900

    test

---
+++ APP_UI/test/test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Bowon Han on 10/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
