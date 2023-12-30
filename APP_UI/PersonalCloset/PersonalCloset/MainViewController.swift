9c6722ef459c258527b7303c4978c1175ea05740
Bowon Han <rabbitlin@naver.com>
2023-12-30 22:41:15 +0900

    Feat: UI pusblishing

---
+++ APP_UI/PersonalCloset/PersonalCloset/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  PersonalCloset
+//
+//  Created by Bowon Han on 12/30/23.
+//
+
+import UIKit
+
+class MainViewController : UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemBackground
+        
+    }
+}
---
+++ APP_UI/PersonalCloset/PersonalCloset/SettingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingViewController.swift
+//  PersonalCloset
+//
+//  Created by Bowon Han on 12/30/23.
+//
+
+import UIKit
+
+class SettingViewController : UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemBackground
+
+    }
+}
